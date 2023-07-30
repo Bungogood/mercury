eef37e963c91deb0d56e27b67cb55c3333a7c72f
Jonathan Good <github@bungogood.com>
2023-07-30 15:08:35 +0100

    Initial diff

--- mercury.go
+++ mercury.go
@@ -2,42 +2,84 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/go-git/go-git/v5"
-	"github.com/joho/godotenv"
+	"github.com/go-git/go-git/v5/plumbing/object"
 )
 
 func openRepo(repoPath string) (*git.Repository, error) {
-	repo, err := git.PlainOpen(repoPath)
+	return git.PlainOpen(repoPath)
+}
+
+func getDiffBetweenCommits(repo *git.Repository, commit1, commit2 *object.Commit) (object.Changes, error) {
+	commit1Tree, err := commit1.Tree()
 	if err != nil {
 		return nil, err
 	}
-	return repo, nil
+
+	commit2Tree, err := commit2.Tree()
+	if err != nil {
+		return nil, err
+	}
+
+	return commit1Tree.Diff(commit2Tree)
 }
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
 	repoPath := "."
 
 	repo, err := openRepo(repoPath)
 	if err != nil {
-		fmt.Println("Error opening repository:", err)
+		fmt.Println("Error opening the repository:", err)
 		os.Exit(1)
 	}
 
-	// Now you have the 'repo' object and can work with the Git repository
-	// For example, you can get the HEAD reference like this:
 	head, err := repo.Head()
 	if err != nil {
-		fmt.Println("Error getting HEAD reference:", err)
+		fmt.Println("Error getting commit objects:", err)
 		os.Exit(1)
 	}
 
-	fmt.Println("HEAD reference:", head.Name())
+	from, err := repo.CommitObject(head.Hash())
+	if err != nil {
+		fmt.Println("Error getting commit objects:", err)
+		os.Exit(1)
+	}
+
+	parent, err := from.Parent(0)
+	if err != nil {
+		fmt.Println("Error getting commit objects:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(from.Hash)
+	fmt.Println(parent.Hash)
+
+	diff, err := getDiffBetweenCommits(repo, from, parent)
+	if err != nil {
+		fmt.Println("Error getting diff between commits:", err)
+		os.Exit(1)
+	}
+
+	// Print the diff data
+	for _, change := range diff {
+		fmt.Println(change)
+		patch, err := change.Patch()
+		if err != nil {
+			fmt.Println("Error getting patch:", err)
+			continue
+		}
+
+		fmt.Println(patch.FilePatches())
+
+		for _, patch := range patch.FilePatches() {
+			// from, to := patch.Files()
+			// fmt.Println(from.Path, to.Path)
+			for index, chunk := range patch.Chunks() {
+				fmt.Println(index, chunk.Content())
+			}
+			fmt.Println()
+		}
+	}
 }
